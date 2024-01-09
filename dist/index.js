17aae1aa3718fd0b9e6d9dc3b449af0ad595b1c6
Sebastian Schepis <sschepis@gmail.com>
2024-01-08 21:39:49 -0700

    updates

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AbiSyncer = exports.GunJSBDIndexer = void 0;
-const abi_syncer_1 = require("./abi-syncer");
-const gun_indexer_1 = require("./gun-indexer");
-module.exports = {
-    GunJSBDIndexer: gun_indexer_1.GunJSBDIndexer,
-    AbiSyncer: abi_syncer_1.AbiSyncer
-};
