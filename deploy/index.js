d336a673b1503bfbbb148d1d5e5a46dfebed093a
Sebastian Schepis <sschepis@gmail.com>
2024-01-08 21:40:03 -0700

    updates

---
+++ deploy/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const abi_syncer_1 = require("./abi-syncer");
+const gun_indexer_1 = require("./gun-indexer");
+module.exports = {
+    AbiSyncer: abi_syncer_1.default,
+    GunJSBDIndexer: gun_indexer_1.default
+};
